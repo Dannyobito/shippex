06bc21ed2d780218221c4c60ee6ab13487690822
Dannyobito <dannyobito05@gmail.com>
2024-11-30 09:15:53 +0100

    improved responsiveness of timeline card on <400px screens

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -136,27 +136,29 @@ const Details = ({ itemDetails }: { itemDetails: any }) => {
         <h2 className="font-bold text-lg">TIMELINE</h2>
         <div className="h-full lg:overflow-y-scroll">
           {itemDetails?.scans?.map((scan: any, i: number) => (
-            <div className="flex justify-start">
+            <div className="flex flex-col sm:flex-row justify-start">
               <div className="flex flex-col font-medium text-sm leading-5 w-[6.3125rem] text-basicGray">
                 <p>{formatDate(scan?.creation).time}</p>
                 <p>{formatDate(scan?.creation).date}</p>
               </div>
-              <div className="flex flex-col items-center">
-                <img src={scanIcons[i % 4]} />
-                <img src={divider} className="h-full" />
-              </div>
-              <div className="flex flex-col gap-1 px-3">
-                <p className="text-base leading-6 font-semibold">
-                  {scan?.scan}
-                </p>
-                <p className="text-base leading-6 font-medium text-basicGray">
-                  {scan?.scan_comment}
-                </p>
-                <div className="flex gap-2 py-[0.625rem]">
-                  <img src={avatar} />
+              <div className="flex gap-2 sm:gap-0">
+                <div className="flex flex-col items-center">
+                  <img src={scanIcons[i % 4]} className="min-h-fit min-w-fit" />
+                  <img src={divider} className="min-w-fit pb-2" />
+                </div>
+                <div className="flex flex-col gap-1 px-3">
                   <p className="text-base leading-6 font-semibold">
-                    {scan?.owner}
+                    {scan?.scan}
                   </p>
+                  <p className="text-base leading-6 font-medium text-basicGray">
+                    {scan?.scan_comment}
+                  </p>
+                  <div className="flex gap-2 py-[0.625rem]">
+                    <img src={avatar} />
+                    <p className="text-base leading-6 font-semibold">
+                      {scan?.owner}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
